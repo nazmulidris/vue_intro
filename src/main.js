a3f7170691988b8bc4060c6582c252fc934e3515
Nazmul Idris <nazmul@fasterlap.com>
2018-08-13 17:13:54 -0700

    Test more capabilities of Vue.js
    - Try out different v-* directives

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 // src/main.js
 
-const app = new Vue({
-          el: '#app',
-          data: {
-            message: '🐵 Monkey balls 🔮'
-          }
-        });
+const vue_app_1 = new Vue(
+  {
+    el  : '#app',
+    data: {
+      message  : '🐵 Monkey balls 🔮',
+      timestamp: `Timestamp ${new Date().toLocaleString()}`,
+    },
+  });
+
+
+const vue_app2 = new Vue(
+  {
+    el  : '#app-2',
+    data: {
+      todos: [
+        {text: 'Learn JavaScript'},
+        {text: 'Learn Vue'},
+        {text: 'Build something awesome'},
+      ],
+    },
+  });
